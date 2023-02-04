c0a947ad5086b6dc73c52ea2f46f17772048b4fd
Myroslava Liubchenko <2829786L@student.gla.ac.uk>
2023-02-04 14:57:45 +0000

    eighth commit

--- rango/views.py
+++ rango/views.py
@@ -111,3 +111,6 @@ def add_page(request, category_name_slug):
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
 
+
+
+
