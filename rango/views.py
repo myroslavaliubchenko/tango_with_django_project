77e4d4b0387bfafa667bca9fe50f5dec4b0bef33
Myroslava Liubchenko <2829786L@student.gla.ac.uk>
2023-02-20 20:25:40 +0000

    11th commit

--- rango/views.py
+++ rango/views.py
@@ -15,14 +15,14 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-
+    context_dict['extra'] = 'From the model solution on GitHub'
     
     visitor_cookie_handler(request)
 
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-
+    
     context_dict = {}
     visitor_cookie_handler(request)
     context_dict['visits'] = request.session['visits']
@@ -66,7 +66,7 @@ def add_page(request, category_name_slug):
     except:
         category = None
     
-    # You cannot add a page to a Category that does not exist... DM
+
     if category is None:
         return redirect(reverse('rango:index'))
 
@@ -84,66 +84,14 @@ def add_page(request, category_name_slug):
 
                 return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
         else:
-            print(form.errors)  # This could be better done; for the purposes of TwD, this is fine. DM.
+            print(form.errors)  
     
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
 
-def register(request):
-    registered = False
-
-    if request.method == 'POST':
-        user_form = UserForm(request.POST)
-        profile_form = UserProfileForm(request.POST)
-
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-
-            profile = profile_form.save(commit=False)
-            profile.user = user
-
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-            
-            profile.save()
-            registered = True
-        else:
-            print(user_form.errors, profile_form.errors)
-    else:
-        user_form = UserForm()
-        profile_form = UserProfileForm()
-    
-    return render(request, 'rango/register.html', context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
-
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(username=username, password=password)
-
-        if user:
-            if user.is_active:
-                login(request, user)
-                return redirect(reverse('rango:index'))
-            else:
-                return HttpResponse("Your Rango account is disabled.")
-        else:
-            print(f"Invalid login details: {username}, {password}")
-            return HttpResponse("Invalid login details supplied.")
-    else:
-        return render(request, 'rango/login.html')
-
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html')
-
-@login_required
-def user_logout(request):
-    logout(request)
-    return redirect(reverse('rango:index'))
 
 def get_server_side_cookie(request, cookie, default_val=None):
     val = request.session.get(cookie)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -39,7 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 ]
+
+# If True, users can register.
+REGISTRATION_OPEN = True
+# If True, the user will be automatically logged in after registering.
+REGISTRATION_AUTO_LOGIN = True
+# The URL that Django redirects users to after logging in.
+LOGIN_REDIRECT_URL = 'rango:index'
+# The page users are directed to if they are not logged in.
+# This was set in a previous chapter. The registration package uses this, too.
+LOGIN_URL = 'auth_login'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
